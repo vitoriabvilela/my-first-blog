639ba2b89b2d9f0df61e7deb3bf9d3bf6a17ad96
Vitória Cristina <vitoriacristinaborba1@gmail.com>
2023-07-20 10:56:59 -0300

    html

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# Este padrão dirá ao Django que views.post_list é o lugar correto aonde ir se alguém entra em seu site pelo endereço
+# 'http://127.0.0.1:8000 /'.
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
